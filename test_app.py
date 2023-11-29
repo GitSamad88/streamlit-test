790b22165860e2d5ce100948e31cef2597478748
ABDESSAMAD TAOUFQ <110288424+GitSamad88@users.noreply.github.com>
2023-11-29 23:03:34 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,8 +3,8 @@ st.title("calculate x * y")
 
 form=st.form(key="test")
 
-X=form.input_number("enter the value of x ", value=1, key = "x")
-Y=form.input_number("enter the value of y ", value=1, key = "y")
+X=form.number_input("enter the value of x ", value=1, key = "x")
+Y=form.number_input("enter the value of y ", value=1, key = "y")
 
 if form.form_submit_button("x * y"):
     form.write(f" x * y = {X*Y}")
