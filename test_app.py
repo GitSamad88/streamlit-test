4aefcc35e028776e1809f401baf75759aff3e523
ABDESSAMAD TAOUFQ <110288424+GitSamad88@users.noreply.github.com>
2023-11-29 22:56:50 +0100

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.title("calculate x * y)
+
+form=st.form(key="test")
+
+X=form.input_number("enter the value of x ", value=1, key = "x")
+Y=form.input_number("enter the value of y ", value=1, key = "y")
+
+if form.form_submit_button("x * y"):
+    form.write(f" x * y = {X*Y}")
